ecabedfa492e13552dd19b843f637ffab1792797
afrid <azar_m@etna-alternance.net>
2024-06-19 14:59:59 +0200

    fix : translation english french

--- django-project/multilang_site/main/main/controllers/articles.py
+++ django-project/multilang_site/main/main/controllers/articles.py
@@ -1,11 +1,12 @@
-# main/views.py
+# # main/views.py
 
-from django.shortcuts import render
-from ..models import Article
+# from django.shortcuts import render
+# from ..models import Article
 
-def article_list(request):
-    articles = Article.objects.all()
-    context = {
-        'articles': articles
-    }
-    return render(request, 'article_list.html', context)
+# def article_list(request):
+#     articles = Article.objects.all()
+#     context = {
+#         'articles': articles
+#     }
+#     return render(request, 'article_list.html', context)
+# # 
