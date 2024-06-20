4cd6a209b5857d0a2788389f7290e359c4416bf3
afrid <azar_m@etna-alternance.net>
2024-06-20 22:01:58 +0200

    fix:probleme de traduction v2

--- main/models.py
+++ main/models.py
@@ -1,9 +1,9 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 class Article(models.Model):
-    title = models.CharField(max_length=200)
+    title = models.CharField(max_length=255, verbose_name=_("Title"))
     content = models.TextField()
     publication_date = models.DateField()
 
-    def __str__(self):
-        return self.title
+    
