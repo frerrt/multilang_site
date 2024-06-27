db781d2697ecc7c9c2903456e99a717324e79a42
afrid <azar_m@etna-alternance.net>
2024-06-27 16:53:49 +0200

    perf:admin

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Article
 
-# Register your models here.
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'published_date')
+    search_fields = ('title', 'content')
+
+# Add more models and customization here as needed
--- main/models.py
+++ main/models.py
@@ -1,12 +1,11 @@
 from django.db import models
-from django.utils.translation import gettext_lazy as _
-from django.utils import timezone
+from django.contrib.auth.models import User
 
 class Article(models.Model):
     title = models.CharField(max_length=200)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField()
-    date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(auto_now_add=True)
 
-  
     def __str__(self):
         return self.title
