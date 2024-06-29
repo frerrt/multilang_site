b441c66d35abc53ad2551e09d7f21f5402bfc0d2
afrid <azar_m@etna-alternance.net>
2024-06-29 16:35:40 +0200

    fix : allowed host22

--- core/settings.py
+++ core/settings.py
@@ -3,12 +3,7 @@ import os
 
 SECRET_KEY = 'django-insecure-rdz8ln37wjjeyhj&s0earj*12!6x(pyfv)#z921tx*w09ctdd*'
 DEBUG = True
-ALLOWED_HOSTS = [   
-    'multilang-site-1-9oaq.onrender.com',
-    'localhost',       # Add localhost for development
-    '127.0.0.1',       # Add localhost IP address for development
-]
-
+ALLOWED_HOSTS = ['multilang-site-1-9oaq.onrender.com', 'localhost', '127.0.0.1', '[::1]']
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # URL de base pour les fichiers statiques
