e05612bfea53e7b4dd5aeadfcfb2266c97b9448c
afrid <azar_m@etna-alternance.net>
2024-06-30 18:41:32 +0200

    fix : disallowed host

--- core/settings.py
+++ core/settings.py
@@ -3,7 +3,7 @@ import os
 
 SECRET_KEY = 'django-insecure-rdz8ln37wjjeyhj&s0earj*12!6x(pyfv)#z921tx*w09ctdd*'
 DEBUG = True
-ALLOWED_HOSTS = ['multilang-site-1-9oaq.onrender.com', 'localhost', '127.0.0.1', '[::1]']
+ALLOWED_HOSTS = ['multilang-site-4-3kvh.onrender.com', 'localhost', '127.0.0.1', '[::1]']
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # URL de base pour les fichiers statiques
